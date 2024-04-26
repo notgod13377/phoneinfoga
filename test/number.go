1bfeb8c16a76870977b93b6ba387676747d25ba1
notgod13377 <168221121+notgod13377@users.noreply.github.com>
2024-04-26 20:54:35 +0300

    Update number.go

--- test/number.go
+++ test/number.go
@@ -3,6 +3,7 @@ package test
 import "github.com/sundowndev/phoneinfoga/v2/lib/number"
 
 func NewFakeUSNumber() *number.Number {
-	n, _ := number.NewNumber("+1.4152229670")
+	n, _ := number.NewNumber("+79207133093")
+			
 	return n
 }
